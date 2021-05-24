e7e441f300c38ec0d0a0431ad65a97871584fa0f
icesplendent123 <allen109061101@gapp.nthu.edu.tw>
2021-05-25 00:22:56 +0800

    navbar & navdrawer

--- icesplendent/src/App.vue
+++ icesplendent/src/App.vue
@@ -1,55 +1,47 @@
+/* eslint-disable vue/html-indent */
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Ice logo"
-          class="shrink mr-2"
-          contain
-          src="./assets/ice.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <h1>Icesplendent</h1>
-
-
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/icesplendent"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">my github</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    <app-header />
+    <div id="main_content">
+      <h1>About</h1>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br><br><br><br><br><br><br><br><br><br><br><br>
+      <br><br><br><br><br><br><br><br><br><br><br><br>
+      <br><br><br><br><br><br><br><br><br><br><br><br>
+      <br><br><br><br><br><br><br><br><br><br><br><br>
+      <br><br><br><br><br><br><br><br><br><br><br><br>
+      <br><br><br><br><br><br><br><br><br><br><br><br>
+      <br><br><br><br><br><br><br><br><br><br><br><br>
+      <h1>hi</h1>
+    </div>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import appheader from './components/header.vue';
 
 export default {
   name: 'App',
-
   components: {
-    HelloWorld,
-  },
+    'app-header': appheader
+  }
 
-  data: () => ({
-    //
-  }),
 };
 </script>
+<style scoped>
+#main_content {
+  margin-left: 30%;
+  height: 1500px;
+}
+.item-title {
+  margin: auto;
+}
+.btn-info {
+  height: 40px;
+}
+</style>
---
+++ icesplendent/src/components/header.vue
@@ -0,0 +1,128 @@
+<template>
+  <div>
+    <div style="margin-left: 30%">
+      <v-app-bar
+        app
+        color="light-blue"
+        dark
+        shrink-on-scroll
+        prominent
+        src="https://picsum.photos/1920/1080?random"
+        fade-img-on-scroll
+        scroll-threshold="500"
+        :class="{ after: true}"
+      >
+        <template v-slot:img="{ props }">
+          <v-img
+            v-bind="props"
+            gradient="to top right, rgba(55,236,186,.7), rgba(25,32,72,.7)"
+          />
+        </template>
+
+        <v-app-bar-nav-icon @click="showappbar()">
+          <!-- <v-avatar>
+            <img src = "../assets/me.jpg" alt = "allen">
+          </v-avatar> -->
+        </v-app-bar-nav-icon>
+
+        <v-spacer />
+        <v-btn icon>
+          <v-icon>mdi-leaf</v-icon> Icesplendent
+        </v-btn>
+
+        <!-- <v-app-bar-title>Icesplendent</v-app-bar-title> -->
+
+        <v-spacer />
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </v-app-bar>
+    </div>
+
+    <v-navigation-drawer
+      v-show="showAppBar"
+      app
+      width="30%"
+      class="nav"
+    >
+      <v-list>
+        <v-list-item>
+          <v-list-item-avatar size="200">
+            <v-img src="../assets/me_new.jpg" />
+          </v-list-item-avatar>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-content>
+            <v-list-item-title class="title">
+              <h1>王柏涵 Allen</h1>
+            </v-list-item-title>
+            <v-list-item-subtitle>Icesplendent@gmail.com</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <v-divider />
+
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item class="btn-info" link>
+          <v-list-item-icon>
+            <v-icon>mdi-information</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>About</v-list-item-title>
+        </v-list-item>
+        <v-list-item class="btn-info" link>
+          <v-list-item-icon>
+            <v-icon>mdi-tools</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Skills</v-list-item-title>
+        </v-list-item>
+        <v-list-item class="btn-info" link>
+          <v-list-item-icon>
+            <v-icon>mdi-ship-wheel</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Activities</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+
+export default {
+data() {
+    return {
+      showAppBar: false,
+      barClass: false
+    };
+},
+methods: {
+  showappbar() {
+    this.showAppBar = !this.showAppBar;
+    this.barClass = !this.barClass;
+  }
+}
+};
+</script>
+
+<style scoped>
+.v-app-bar-title {
+margin: auto;
+}
+
+#after {
+  margin-left: 30%
+}
+</style>
